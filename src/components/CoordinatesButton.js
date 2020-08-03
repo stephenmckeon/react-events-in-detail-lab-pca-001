011aa37776d98e7da59492024de04b4bbeb3b66a
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-08-03 15:32:21 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,21 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+  render() {
+    return (
+      <button
+        onClick={this.handleClick}
+      ></button>
+    )
+  }
+
+  handleClick = event => {
+    const x = event.clientX
+    const y = event.clientY
+
+    this.props.onReceiveCoordinates([x, y])
+  }
+
+
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,17 @@
-// Code DelayedButton Component Here
+// Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+  render() {
+    return (
+      <button
+        onClick={this.handleClick}
+      ></button>
+    )
+  }
+
+  handleClick = event => {
+    event.persist()
+    setTimeout( () => {this.props.onDelayedClick(event)}, this.props.delay )
+  }
+}
